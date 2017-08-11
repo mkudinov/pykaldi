be61fedbca85986d6de6614594c4f04e335748dc
mkudinov <mikhailkudinov@gmail.com>
2017-08-11 16:46:48 +0300

    utility function in a separate file (refactor)

---
+++ readers/utilities.py
@@ -0,0 +1,287 @@
+import cffi
+import numpy as np
+import os
+import pdb
+import sys
+
+if not os.path.isfile('common/constants.py'):
+    print "No import module found in the PYTHONPATH. Please, add current directory to your PYTHON_PATH"
+    exit(1)
+
+from common.constants import KALDI_ERR_CODES as ked
+from common.constants import print_error as print_error
+
+ffi = None
+kaldi_lib = None
+
+KALDI_PATH = '/home/mkudinov/KALDI/kaldi_new/kaldi/'
+RUSPEECH_EXP_PATH = 'egs/ruspeech/s1/'
+ALIGNS_PATH = 'exp/tri1/ali.1.gz'
+FEATURE_PATH = 'mfcc/raw_mfcc_train.1.ark'
+LIB_PATH = 'libpython-kaldi-readers.so'
+PATH_TO_TRANSITION_MODEL = KALDI_PATH + RUSPEECH_EXP_PATH + 'exp/tri1/final.mdl'
+PATH_TO_PHONES_TABLE = KALDI_PATH + RUSPEECH_EXP_PATH + 'data/lang/phones.txt'
+
+def initialize_cffi():
+    src = """
+    typedef struct 
+    {
+        int number_of_phones;
+        int *phones;
+        int *num_repeats_per_phone;
+    } Alignment;  
+    /* AlignmentReader */
+    void *GetAlignmentReader(char *i_specifier
+                      ,  int *o_err_code);
+    void DeleteAlignmentReader(void *o_alignment_reader);
+    Alignment *ReadAlignment(char *i_key
+                       , void *i_transition_model
+                       , void *i_alignment_reader
+                       , int *o_err_code);
+
+    /* TransitionModel*/
+    void *GetTransitionModel(char *i_transition_model_filename
+                       , int *o_err_code);
+    void DeleteTransitionModel(void *o_transition_model);
+
+    /* Acoustic model*/
+    void *GetAcousticModel(char *i_model_filename
+                     , int *o_err_code);
+    void DeleteAcousticModel(void *o_amm_gmm);
+
+    /* ContextTree*/
+    void *GetContextTree(char *i_specifier
+                   , int *o_err_code);
+    void DeleteContextTree(void *o_context_tree);
+
+    /* FeatureReader*/
+    void *GetFeatureReader(char *i_specifier
+                     , int *o_err_code);
+    void DeleteFeatureReader(void *o_feature_reader);
+
+    /* FeatureMatrix */
+    //Non-possessive pointer to feature matrix
+    const void *ReadFeatureMatrix(char *i_key
+                            , void *i_feature_reader
+                            , int *o_n_rows
+                            , int *o_n_columns
+                            , int *o_err_code);
+    void CopyFeatureMatrix(void *i_source
+                     , void *o_destination
+                     , int *o_err_code);
+
+    /* IntegerVector*/
+    int *ReadIntegerVector(char *i_specifier
+                     , int *o_n_elements
+                     , int *o_err_code);
+    void DeleteIntegerVector(int *o_vector);
+    void CopyIntegerVector(int *i_source
+                     , int i_size
+                     , void *o_destination
+                     , int *o_err_code);
+
+    void DeleteAlignment(Alignment *o_alignment_buffer);
+    """
+    global ffi
+    global kaldi_lib
+    ffi = cffi.FFI()
+    ffi.cdef(src)
+    try:
+        kaldi_lib = ffi.dlopen(LIB_PATH)
+    except ImportError:
+        print "Library {} is not found in the LD_LIBRARY_PATH. Please, add ./lib to your LD_LIBRARY_PATH".format(LIB_PATH)
+        exit(1)
+
+class KaldiAlignmentReader(object):
+    def __init__(self, path_to_transition_model, path_to_phones_table):
+        self._kaldi_lib = kaldi_lib
+        self._ffi = ffi
+        self._ptr_last_err_code = self._ffi.new("int*")
+        self._open = False
+        self._result_buffer = None 
+        self._transition_model = self._kaldi_lib.GetTransitionModel(path_to_transition_model, self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0]
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError('Error trying to read transition model from file {}'.format(path_to_transition_model))
+        self._phone_table = []
+        for line in open(path_to_phones_table, 'r'):
+            phone, phone_id = line.split(' ')
+            self._phone_table.append(phone)
+        if len(self._phone_table) == 0:
+            raise RuntimeError('Empty phone table read from file {}'.format(path_to_phones_table))
+
+    def __del__(self):
+        if self._open:
+            self.close_archive()
+        self._kaldi_lib.DeleteTransitionModel(self._transition_model)
+
+    def get_alignment(self, record_descriptor):
+        if not self._open:
+            return None
+        self._result_buffer = self._kaldi_lib.ReadAlignment(record_descriptor, self._transition_model, self._alignment_reader, self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0]
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError('Error trying to get alignment for descriptor {}'.format(record_descriptor))
+        alignment_size = self._result_buffer.number_of_phones
+        alignment = []
+        for phone_num in range(alignment_size):
+            phone_id = self._result_buffer.phones[phone_num] 
+            phone_length = self._result_buffer.num_repeats_per_phone[phone_num]
+            alignment.append((phone_id, self._phone_table[phone_id], phone_length))
+        self._kaldi_lib.DeleteAlignment(self._result_buffer)
+        self._result_buffer = None
+        return alignment
+
+    def open_archive(self, path_to_archive):
+        if self._open:
+            self.close_archive()
+        if not os.path.isfile(path_to_archive):
+            raise RuntimeError('Error trying to open archive {}. No such file or directory'.format(path_to_archive))
+        specifier = "ark,s:gunzip -c {}|".format(path_to_archive)
+        self._alignment_reader = self._kaldi_lib.GetAlignmentReader(specifier, self._ptr_last_err_code)
+        self._open = True
+
+    def close_archive(self):
+        if self._open:
+            self._kaldi_lib.DeleteAlignmentReader(self._alignment_reader)
+        self._open = False
+
+
+class KaldiMatrixProxy(object):
+    def __init__(self, ptr_to_matrix, shape):
+        self._kaldi_lib = kaldi_lib
+        self._ffi = ffi
+        self._ptr_to_matrix = ptr_to_matrix
+        self.shape = shape
+        self.valid = True
+        print shape
+        self._ptr_last_err_code = self._ffi.new("int *")
+
+    def numpy_array(self):
+        if not self.valid:
+            raise RuntimeError("Matrix proxy for object {} is not longer valid!".format(self._ptr_to_matrix[0]))
+        numpy_matrix = np.zeros(self.shape, dtype=np.float32)
+        self._kaldi_lib.CopyFeatureMatrix(self._ptr_to_matrix, self._ffi.cast('void *', numpy_matrix.__array_interface__['data'][0]), self._ptr_last_err_code)
+        return numpy_matrix
+
+
+class KaldiFeatureReader(object):
+    def __init__(self):
+        self._kaldi_lib = kaldi_lib
+        self._ffi = ffi
+        self._ptr_last_err_code = self._ffi.new("int *")
+        self._ptr_return_by_reference1 = self._ffi.new("int *")
+        self._ptr_return_by_reference2 = self._ffi.new("int *")
+        self._open = False
+        self._kaldi_matrices = []
+
+    def __del__(self):
+        if self._open:
+            self.close_archive()
+
+    def get_feature_matrix(self, record_descriptor):
+        result_ptr = self._kaldi_lib.ReadFeatureMatrix(record_descriptor, self._feature_reader, self._ptr_return_by_reference1, self._ptr_return_by_reference2, self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0]
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError('Error trying to get feature matrix for descriptor {}'.format(record_descriptor))
+        num_rows = self._ptr_return_by_reference1[0]
+        num_columns = self._ptr_return_by_reference2[0]
+        feature_matrix = KaldiMatrixProxy(result_ptr, [num_rows, num_columns])  
+        self._kaldi_matrices.append(feature_matrix)
+        return feature_matrix
+
+    def open_archive(self, path_to_archive):
+        if self._open:
+            self.close_archive()
+        if not os.path.isfile(path_to_archive):
+            raise RuntimeError('Error trying to open archive {}. No such file or directory'.format(path_to_archive))
+        specifier = "ark:{}".format(path_to_archive)
+        self._feature_reader = self._kaldi_lib.GetFeatureReader(specifier, self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0] 
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError('Error trying to open archive {}'.format(path_to_archive))
+        self._open = True
+
+    def close_archive(self):
+        if self._open:
+            self._destroy_all_matrices()
+            self._kaldi_lib.DeleteFeatureReader(self._feature_reader)
+        self._open = False
+
+    def _destroy_all_matrices(self):
+        for matrix in self._kaldi_matrices:
+            matrix.valid = False
+        self._kaldi_matrices = []
+
+
+class ReaderUtilities(object):
+    @staticmethod
+    def read_transition_model(path_to_transition_model):
+        ptr_last_err_code = ffi.new("int *")
+        transition_model_handler = kaldi_lib.GetTransitionModel(path_to_transition_model, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            return None
+        return transition_model_handler
+    
+    @staticmethod
+    def delete_transition_model(transition_model_handler):
+        kaldi_lib.DeleteTransitionModel(transition_model_handler)
+    
+    @staticmethod
+    def read_acoustic_model(path_to_model):
+        ptr_last_err_code = ffi.new("int *")
+        model_handler = kaldi_lib.GetAcousticModel(path_to_transition_model, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            return None
+        return model_handler
+    
+    @staticmethod
+    def delete_acoustic_model(model_handler):
+        kaldi_lib.DeleteAcousticModel(model_handler)
+
+    @staticmethod
+    def read_context_tree(path_to_tree_file):
+        ptr_last_err_code = ffi.new("int *")
+        ctx_handler = kaldi_lib.GetContextTree(path_to_tree_file, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            return None
+        return ctx_handler
+    
+    @staticmethod
+    def delete_context_tree(ctx_handler):
+        kaldi_lib.DeleteContextTree(ctx_handler)
+
+    @staticmethod
+    def read_integer_vector(read_specifier):
+        ptr_last_err_code = ffi.new("int *")
+        ptr_n_elements=ffi.new("int *")
+        vector_handler = kaldi_lib.ReadIntegerVector(read_specifier, ptr_n_elements, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            return None
+        return vector_handler, ptr_n_elements[0]
+    
+    @staticmethod
+    def delete_integer_vector(vector_handler):
+        kaldi_lib.DeleteIntegerVector(vector_handler)
+
+
+initialize_cffi()
+if __name__ == '__main__':
+    alignment_reader = KaldiAlignmentReader(PATH_TO_TRANSITION_MODEL, PATH_TO_PHONES_TABLE)
+    path_to_archive = KALDI_PATH + RUSPEECH_EXP_PATH + ALIGNS_PATH
+    alignment_reader.open_archive(path_to_archive)
+    alignment = alignment_reader.get_alignment("TRAIN-FCT018-018R0070")
+    print alignment
+    feature_matrix_reader = KaldiFeatureReader()
+    path_to_feature_archive = KALDI_PATH + RUSPEECH_EXP_PATH + FEATURE_PATH
+    feature_matrix_reader.open_archive(path_to_feature_archive)
+    feature_matrix = feature_matrix_reader.get_feature_matrix("TRAIN-FCT002-002B0181")
+    print feature_matrix.numpy_array()
--- segmentation/align_wav.py
+++ segmentation/align_wav.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 import cffi
 import numpy as np
 import os
@@ -11,6 +12,7 @@ if not os.path.isfile('common/constants.py'):
 from common.constants import KALDI_ERR_CODES as ked
 from common.constants import print_error as print_error
 from readers.utilities import ReaderUtilities as kaldi_reader
+from readers.utilities import KaldiFeatureReader
 
 ffi = None
 kaldi_lib = None
@@ -20,6 +22,12 @@ RUSPEECH_EXP_PATH = 'egs/ruspeech/s1/'
 FEATURE_PATH = 'mfcc/raw_mfcc_train.1.ark'
 LIB_PATH = 'libpython-kaldi-segmentation.so'
 PATH_TO_TRANSITION_MODEL = KALDI_PATH + RUSPEECH_EXP_PATH + 'exp/tri1/final.mdl'
+PATH_TO_PHONES_TABLE = KALDI_PATH + RUSPEECH_EXP_PATH + 'data/lang/phones.txt'
+PATH_TO_TRIPHONE_MODEL = KALDI_PATH + RUSPEECH_EXP_PATH + 'exp/tri1/tree'
+PATH_TO_DISAMBIGUATION_SYMBOLS = KALDI_PATH + RUSPEECH_EXP_PATH + 'data/lang/phones/disambig.int'
+PATH_TO_LEXICAL_FST = KALDI_PATH + RUSPEECH_EXP_PATH + 'data/lang/L.fst'
+TEST_PHRASE_CODE = 'TRAIN-FCT002-002B0181'
+TEST_PHRASE = u'уха обычно готовится из пресноводных рыб'
 PATH_TO_PHONES_TABLE = KALDI_PATH + RUSPEECH_EXP_PATH + 'data/lang/phones.txt'
 
 def initialize_cffi():
@@ -73,51 +81,171 @@ def initialize_cffi():
 class IntegerVector(object):
     def __init__(self, vec_handler=None, n_elements=None, filename=None):
         if vec_handler is not None and n_elements is not None:
-            self.handler = vec_handler
-            self.n_elements = n_elements
+            self._handler = vec_handler
+            self._n_elements = n_elements
         elif filename is not None:
-            self.handler, self.n_elements = kaldi_reader.read_integer_vector(filename)
+            self._handler, self._n_elements = kaldi_reader.read_integer_vector(filename)
         else:
             raise RuntimeError("Constructor arguments are missing")
 
     def __del__(self):
-        kaldi_reader.delete_integer_vector(self.handler)
+        kaldi_reader.delete_integer_vector(self._handler)
+
+    @property
+    def handler(self):
+        return self._handler
+
+    @property
+    def n_elements(self):
+        return self._n_elements
+
 
 
 class ContextTree(object):
     def __init__(self, ctx_handler=None, filename=None):
         if ctx_handler is not None:
-            self.handler = ctx_handler
+            self._handler = ctx_handler
         elif filename is not None:
-            self.handler = kaldi_reader.read_context_tree(filename)
+            self._handler = kaldi_reader.read_context_tree(filename)
         else:
             raise RuntimeError("Constructor arguments are missing")
 
     def __del__(self):
-        kaldi_reader.delete_context_tree(self.handler)
+        kaldi_reader.delete_context_tree(self._handler)
+
+    @property
+    def handler(self):
+        return self._handler
+
 
 class TransitionModel(object):
-    def __init__(selfi, model_handler=None, filename=None):
+    def __init__(self, model_handler=None, filename=None):
         if model_handler is not None:
-            self.handler = model_handler
+            self._handler = model_handler
         elif filename is not None:
-            self.handler = kaldi_reader.read_transition_model(filename)
+            self._handler = kaldi_reader.read_transition_model(filename)
         else:
             raise RuntimeError("Constructor arguments are missing")
 
     def __del__(self):
-        kaldi_reader.read_transition_model(filename)
+        kaldi_reader.delete_transition_model(self._handler)
+
+    @property
+    def handler(self):
+        return self._handler
 
 
-class KaldiWavAligner(object):
-    def __init__(self):
-        self.kaldi_lib = kaldi_lib
-        self.ffi = ffi
-        self.kaldi_lib.GetAligner(path_to_context_tree, path_to_transition_model, path_to_lexicon_fst)
+class PhraseMatcher(object):
+    def __init__(self, text_fst_compiler, text_codes):
+        self._kaldi_lib = kaldi_lib
+        self._ffi = ffi
+        self._text_codes = self._ffi.new("int[]", text_codes)
+        self._ptr_last_err_code = self._ffi.new("int *")
+        self._aligner = self._kaldi_lib.GetAlignerFst(text_fst_compiler.handler, self._text_codes, len(text_codes), self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0]
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError("Error creating aligner")
+
+    @property
+    def text(self):
+        return self._text
+
+    def __del__(self):
+        self._kaldi_lib.DeleteAligner(self._aligner)
+
+
+class TextFstCompiler(object):
+    def __init__(self, path_to_lexicon_fst, context_tree, disambiguation_symbols, transition_model):
+        self._kaldi_lib = kaldi_lib
+        self._ffi = ffi
+        self._ptr_last_err_code = self._ffi.new("int *")
+        self.handler = self._kaldi_lib.GetTextFstCompiler(context_tree.handler, disambiguation_symbols.handler, disambiguation_symbols.n_elements, transition_model.handler, path_to_lexicon_fst, self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0]
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError("Error creating text fst compiler")
+
+    def __del__(self):
+         self._kaldi_lib.DeleteTextFstCompiler(self.handler)
+
+
+class WavAligner(object):
+    def __init__(self, text_fst_compiler, transition_model, acoustic_model, char_codes):
+        self._fst_compiler = text_fst_compiler
+        self._transition_model = transition_model
+        self._acoustic_model = acoustic_model
+        self._phrases = {}
+        self._kaldi_lib = kaldi_lib
+        self._ffi = ffi
+        self._char_codes = char_codes
+        self._ptr_last_err_code = self._ffi.new("int *")
+
+    def add_phrase(self, text, transcription):
+        self._phrases[text] = PhraseMatcher(self._fst_compiler, transcription)
+
+    def align(self, mfcc_features, text, acoustic_scale=1.0, transition_scale=1.0, self_loop_scale=1.0, beam=10, retry_beam=40):
+        alignment = self._kaldi_lib.Align(mfcc_features.handler, self._transition_model.handler, self._acoustic_model, self._phrases[text], acoustic_scale, transition_scale, self_loop_scale, beam, retry_beam, False, self._ptr_last_err_code)
+        err_code = self._ptr_last_err_code[0]
+        if err_code != ked.OK:
+            print_error(err_code)
+            raise RuntimeError("Error while aligning")
+        return alignment
+
+    @property
+    def n_phrases(self):
+        return len(self._phrases)
+
+
+class Transcriber(object):
+    def __init__(self, word_table, unknown_word_code, phone_table, inv_phone_table, encoding='default'):
+        self._word_table = word_table
+        assert(self._unk_code in self._word_table)
+        self._unk_code = self._word_table[unknown_word_code]
+        self._code_to_char = phone_table
+        self._char_to_int = inv_phone_table
+        self._encoding = encoding
+
+    def transcribe(phrase):
+        if self._encoding != 'default':
+            phrase = phrase.encode(self.encoding)
+        return [self._word_table[word] if word in self._word_table else self.unk_code for word in phrase.split(' ') ]
+
+
+def load_phones_table(path_to_phones_table):
+    phone_table = []
+    inv_phone_table = {}
+    for line in open(path_to_phones_table, 'r'):
+        phone, phone_id = unicode(line.strip()).split(' ')
+        phone_table.append(phone)
+        inv_phone_table[phone] = phone_id
+    if len(phone_table) == 0:
+        raise RuntimeError('Empty phone table read from file {}'.format(path_to_phones_table))
+    return phone_table, inv_phone_table
+
+
+def load_words_table(path_to_word_table):
+    word_table = {}
+    for line in open(path_to_word_table, 'r'):
+        word_code = line.split(' ')
+        word = word_code[0]
+        code = word_code[1]
+        word_table[word] = code
+    return word_table
 
 
 initialize_cffi()
-context_tree = ContextTree(filename=PATH_TO_TRIPHONE_MODEL)
-disambiguation_symbols = IntegerVector(filename=PATH_TO_DISAMBIGUATION_SYMBOLS) 
-transition_model = TransitionModel(filename=PATH_TO_TRANSITION_MODEL)
-text_fst_compiler = TextFstCompiler(PATH_TO_LEXICAL_FST, context_tree, disambiguation_symbols, transition_model)
+if __name__ == '__main__':
+    context_tree = ContextTree(filename=PATH_TO_TRIPHONE_MODEL)
+    disambiguation_symbols = IntegerVector(filename=PATH_TO_DISAMBIGUATION_SYMBOLS) 
+    transition_model = TransitionModel(filename=PATH_TO_TRANSITION_MODEL)
+    text_fst_compiler = TextFstCompiler(PATH_TO_LEXICAL_FST, context_tree, disambiguation_symbols, transition_model)
+    code_to_char, char_to_code = load_phones_table(PATH_TO_PHONES_TABLE)
+    word_to_code = load_word_table(PATH_TO_WORD_TABLE)
+    aligner = WavAligner(text_fst_compiler, transition_model, context_tree, char_to_code)
+    transcriber = Transcriber(word_to_code, UNK_CODE, code_to_char, char_to_code)
+    aligner.add_phrase(TEST_PHRASE, transcriber.transcribe(TEST_PHRASE))
+    path_to_feature_archive = KALDI_PATH + RUSPEECH_EXP_PATH + FEATURE_PATH
+    feature_matrix_reader.open_archive(path_to_feature_archive)
+    feature_matrix = feature_matrix_reader.get_feature_matrix(TEST_FEATURES)
+    aligner.align(features, text)
