f045d5c0a98e973c309480c6abd4e644d6f00175
mkudinov <mikhailkudinov@gmail.com>
2017-09-26 21:18:01 +0300

    ASR model python class

--- asr_model/asr_model.py
+++ asr_model/asr_model.py
@@ -18,7 +18,36 @@ LIB_PATH = 'libpython-kaldi-asr-model.so'
 
 def initialize_cffi():
     src = """
+       /* TransitionModel*/
+        void *GetTransitionModel(char *i_transition_model_filename
+                                , int *o_err_code);
 
+        void DeleteTransitionModel(void *o_transition_model);
+
+        int GetNumberOfTransitionIds(void *i_transition_model
+                                    , int *o_err_code);
+
+        int GetNumberOfPdfsTM(void *i_transition_model
+                             , int *o_err_code);
+
+        int GetNumberOfPhones(void *i_transition_model
+                             , int *o_err_code);
+
+        /* Acoustic model*/
+        void *GetAcousticModel(char *i_transition_model_filename
+                              , int *o_err_code);
+
+        void DeleteAcousticModel(void *o_amm_gmm);
+
+        int GetNumberOfPdfsAM(void *i_acoustic_model
+                             , int *o_err_code);
+
+        int GetNumberOfGauss(void *i_acoustic_model
+                            , int *o_err_code);
+
+        int GetNumberOfGaussInPdf(void *i_acoustic_model
+                                 , int i_pdf_id
+                                 , int *o_err_code);
     """
     global ffi
     global kaldi_lib
@@ -29,3 +58,43 @@ def initialize_cffi():
     except ImportError:
         print "Library {} is not found in the LD_LIBRARY_PATH. Please, add ./lib to your LD_LIBRARY_PATH".format(LIB_PATH)
         exit(1)
+
+class ASR_model(object):
+    def __init__(self, path_to_transition_model=None):
+        self.kaldi_lib = kaldi_lib
+        if path_to_transition_model is None:
+            raise RuntimeError('Only reading from file is currently supported!')
+        ptr_last_err_code = ffi.new("int *")
+        self._ptr_transition_model = self.kaldi_lib.GetTransitionModel(path_to_transition_model, ptr_last_err_code)
+        self._ptr_acoustic_model = self.kaldi_lib.GetAcousticModel(path_to_transition_model, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            raise RuntimeError('Transition model reading failed')
+        self.n_transitions = self.kaldi_lib.GetNumberOfTransitionIds(self._ptr_transition_model, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            raise RuntimeError('Model reference crashed')
+        self.n_pdfs = self.kaldi_lib.GetNumberOfPdfsAM(self._ptr_acoustic_model, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            raise RuntimeError('Model reference crashed')
+        self.n_gauss = self.kaldi_lib.GetNumberOfGauss(self._ptr_acoustic_model, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            raise RuntimeError('Model reference crashed')
+
+    def __del__(self):
+        self.kaldi_lib.DeleteTransitionModel(self._ptr_transition_model)
+        self.kaldi_lib.DeleteAcousticModel(self._ptr_acoustic_model)
+
+    def __str__(self):
+        return "Model: %s transitions; %s pdfs; %s gauss" % (self.n_transitions, self.n_pdfs, self.n_gauss)
+
+
+initialize_cffi()
+if __name__ == '__main__':
+    KALDI_PATH = '/home/mkudinov/KALDI/kaldi_new/kaldi/'
+    RUSPEECH_EXP_PATH = 'egs/ruspeech/s1/'
+    PATH_TO_MODEL = KALDI_PATH + RUSPEECH_EXP_PATH + 'exp/tri1/final.mdl'
+    asr_model = ASR_model(PATH_TO_MODEL)
+    print asr_model
---
+++ asr_model/kaldi-python-asr-model.cc
@@ -0,0 +1,179 @@
+#include "kaldi-python-asr-model.h"
+#include <iostream>
+extern "C"
+{
+namespace kaldi_python_asr_model
+{
+void *GetTransitionModel(char *i_asr_filename, int *o_err_code)
+{
+    *o_err_code = OK;
+    kaldi::TransitionModel* transition_model = new kaldi::TransitionModel();
+    kaldi::ReadKaldiObject(i_asr_filename, transition_model);
+    if(!transition_model)
+    {
+        *o_err_code = ERROR_OPENING;
+        return 0;
+    }
+    return transition_model;
+}
+
+void DeleteTransitionModel(void *o_transition_model)
+{
+    if(o_transition_model)
+    {
+        delete static_cast<kaldi::TransitionModel*>(o_transition_model);
+        o_transition_model = 0;
+    }
+}
+
+int GetNumberOfTransitionIds(void *i_transition_model, int *o_err_code)
+{
+    if(!i_transition_model)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int numbert_of_transition_ids = 0;
+    try
+    {
+        numbert_of_transition_ids = static_cast<kaldi::TransitionModel*>(i_transition_model)->NumTransitionIds();
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return numbert_of_transition_ids;
+}
+
+int GetNumberOfPdfsTM(void *i_transition_model, int *o_err_code)
+{
+    if(!i_transition_model)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int number_of_pdfs = 0;
+    try
+    {
+        number_of_pdfs = static_cast<kaldi::TransitionModel*>(i_transition_model)->NumPdfs();
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return number_of_pdfs;
+}
+
+int GetNumberOfPhones(void *i_transition_model, int *o_err_code)
+{
+    if(!i_transition_model)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int number_of_phones = 0;
+    try
+    {
+        number_of_phones = static_cast<kaldi::TransitionModel*>(i_transition_model)->NumPhones();
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return number_of_phones;
+}
+
+void *GetAcousticModel(char *i_asr_model_filename, int *o_err_code)
+{
+    *o_err_code = OK;
+    kaldi::TransitionModel transition_model;
+    std::string model_filename(i_asr_model_filename);
+    kaldi::AmDiagGmm* am_gmm = new kaldi::AmDiagGmm();
+    {
+        bool binary;
+        kaldi::Input ki(model_filename, &binary);
+        transition_model.Read(ki.Stream(), binary);
+        am_gmm->Read(ki.Stream(), binary);
+    }
+    if(!am_gmm)
+    {
+        *o_err_code = ERROR_OPENING;
+        return 0;
+    }
+    return am_gmm;
+}
+
+void DeleteAcousticModel(void *o_am_gmm)
+{
+    if(o_am_gmm )
+    {
+        delete static_cast<kaldi::AmDiagGmm*>(o_am_gmm);
+        o_am_gmm = 0;
+    }
+}
+
+int GetNumberOfPdfsAM(void *i_acoustic_model, int *o_err_code)
+{
+    if(!i_acoustic_model)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int number_of_pdfs = 0;
+    try
+    {
+        number_of_pdfs = static_cast<kaldi::AmDiagGmm*>(i_acoustic_model)->NumPdfs();
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return number_of_pdfs;
+}
+
+int GetNumberOfGauss(void *i_acoustic_model, int *o_err_code)
+{
+    if(!i_acoustic_model)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int number_of_gaussian = 0;
+    try
+    {
+        number_of_gaussian = static_cast<kaldi::AmDiagGmm*>(i_acoustic_model)->NumGauss();
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    std::cout << static_cast<kaldi::AmDiagGmm*>(i_acoustic_model)->NumGauss() << std::endl;
+    return number_of_gaussian;
+}
+
+int GetNumberOfGaussInPdf(void *i_acoustic_model, int i_pdf_id, int *o_err_code)
+{
+    if(!i_acoustic_model)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int number_of_gaussian = 0;
+    try
+    {
+        number_of_gaussian = static_cast<kaldi::AmDiagGmm*>(i_acoustic_model)->NumGaussInPdf(i_pdf_id);
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return number_of_gaussian;
+}
+} //namespace kaldi_python_asr_model
+} //extern "C"
