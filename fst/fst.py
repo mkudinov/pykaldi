7e44f1b33209557028ea4dc9a57b51cac1c737d0
mkudinov <mikhailkudinov@gmail.com>
2017-09-27 18:47:38 +0300

    FST python class

--- fst/fst.py
+++ fst/fst.py
@@ -17,7 +17,10 @@ kaldi_lib = None
 LIB_PATH = 'libpython-kaldi-fst.so'
 
 def initialize_cffi():
-    src = """;
+    src = """
+        void *GetFst(char *i_path_to_fst, int *o_err_code);
+        int GetNumberOfArcs(void *i_fst, int *o_err_code);
+        void DeleteFst(void *o_fst);
     """
     global ffi
     global kaldi_lib
@@ -30,10 +33,32 @@ def initialize_cffi():
         exit(1)
 
 
+class KaldiFST(object):
+    def __init__(self, path_to_fst_file):
+        self.kaldi_lib = kaldi_lib
+        if path_to_fst_file is None:
+            raise RuntimeError('Only reading from file is currently supported!')
+        ptr_last_err_code = ffi.new("int *")
+        self._ptr_fst = self.kaldi_lib.GetFst(path_to_fst_file, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            raise RuntimeError('FST reading failed')
+        self.n_arcs = self.kaldi_lib.GetNumberOfArcs(self._ptr_fst, ptr_last_err_code)
+        err_code = ptr_last_err_code[0]
+        if err_code != ked.OK:
+            raise RuntimeError('FST reference crashed')
+
+    def __del__(self):
+        self._ptr_fst = self.kaldi_lib.DeleteFst(self._ptr_fst)
+
+    def __str__(self):
+        return "FST: %s arcs" % (self.n_arcs)
+
+
 initialize_cffi()
 if __name__ == '__main__':
     KALDI_PATH = '/home/mkudinov/KALDI/kaldi_new/kaldi/'
     RUSPEECH_EXP_PATH = 'egs/ruspeech/s1/'
-    PATH_TO_MODEL = KALDI_PATH + RUSPEECH_EXP_PATH + 'exp/tri1/final.mdl'
-    asr_model = ASR_model(PATH_TO_MODEL)
-    print asr_model
+    PATH_TO_LEXICAL_FST = KALDI_PATH + RUSPEECH_EXP_PATH + 'data/lang/L.fst'
+    fst = KaldiFST(PATH_TO_LEXICAL_FST)
+    print fst
--- fst/kaldi-python-fst.cc
+++ fst/kaldi-python-fst.cc
@@ -4,6 +4,49 @@ extern "C"
 {
 namespace kaldi_python_fst
 {
+void *GetFst(char *i_path_to_fst, int *o_err_code)
+{
+    *o_err_code = OK;
+    fst::VectorFst<fst::StdArc> *fst = fst::ReadFstKaldi(i_path_to_fst);
+    if(!fst)
+    {
+        *o_err_code = ERROR_OPENING;
+        return 0;
+    }
+    return fst;
+}
 
+void DeleteFst(void *o_fst)
+{
+    if(o_fst)
+    {
+        delete static_cast<fst::VectorFst<fst::StdArc>*>(o_fst);
+        o_fst = 0;
+    }
+}
+
+int GetNumberOfArcs(void *i_fst, int *o_err_code)
+{
+    if(!i_fst)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int number_of_arcs = 0;
+    try
+    {
+        fst::VectorFst<fst::StdArc> *fst = static_cast<fst::VectorFst<fst::StdArc>*>(i_fst);
+        for (int s = 0; s < fst->NumStates(); s++)
+        {
+            number_of_arcs += fst->NumArcs(s);
+        }
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return number_of_arcs;
+}
 } //namespace kaldi_python_fst
 } //extern "C"
