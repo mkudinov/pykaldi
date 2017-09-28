dcccd060d8ef93d6446ec1d45043ec56344475fc
mkudinov <mikhailkudinov@gmail.com>
2017-09-28 14:59:33 +0300

    ContextDependency python class

--- tree/kaldi-python-tree.cc
+++ tree/kaldi-python-tree.cc
@@ -1,4 +1,4 @@
-#include "kaldi-python-fst.h"
+#include "kaldi-python-tree.h"
 #include <iostream>
 extern "C"
 {
@@ -7,7 +7,8 @@ namespace kaldi_python_tree
 void *GetContextDependency(char *i_path_to_tree, int *o_err_code)
 {
     *o_err_code = OK;
-    kaldi::ContextDependency *tree = kaldi::ReadKaldiObject(i_path_to_tree);
+    kaldi::ContextDependency *tree = new kaldi::ContextDependency();
+    kaldi::ReadKaldiObject(i_path_to_tree, tree);
     if(!tree)
     {
         *o_err_code = ERROR_OPENING;
@@ -25,28 +26,44 @@ void DeleteContextDependency(void *o_tree)
     }
 }
 
-int GetNumberOfArcs(void *i_fst, int *o_err_code)
+int GetContextWidth(void *i_tree, int *o_err_code)
 {
-    if(!i_fst)
+    if(!i_tree)
     {
         *o_err_code = CORRUPTED_POINTER;
         return 0;
     }
     *o_err_code = OK;
-    int number_of_arcs = 0;
+    int context_length = 0;
     try
     {
-        fst::VectorFst<fst::StdArc> *fst = static_cast<fst::VectorFst<fst::StdArc>*>(i_fst);
-        for (int s = 0; s < fst->NumStates(); s++)
-        {
-            number_of_arcs += fst->NumArcs(s);
-        }
+        context_length = static_cast<kaldi::ContextDependency*>(i_tree)->ContextWidth();
     }
     catch(...)
     {
         *o_err_code = CORRUPTED_POINTER;
     }
-    return number_of_arcs;
+    return context_length;
+}
+
+int GetCentralPosition(void *i_tree, int *o_err_code)
+{
+    if(!i_tree)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+        return 0;
+    }
+    *o_err_code = OK;
+    int central_position = 0;
+    try
+    {
+        central_position = static_cast<kaldi::ContextDependency*>(i_tree)->CentralPosition();
+    }
+    catch(...)
+    {
+        *o_err_code = CORRUPTED_POINTER;
+    }
+    return central_position;
 }
 } //namespace kaldi_python_tree
 } //extern "C"
--- tree/kaldi-python-tree.h
+++ tree/kaldi-python-tree.h
@@ -8,7 +8,7 @@
 #include "common/kaldi-python-common.h"
 #include "../base/kaldi-common.h"
 #include "../util/common-utils.h"
-#include "../fstext/kaldi-fst-io.h"
+#include "../tree/context-dep.h"
 
 using namespace kaldi_python_common_errors;
 extern "C"
@@ -16,7 +16,8 @@ extern "C"
 namespace kaldi_python_tree
 {
     void *GetContextDependency(char *i_path_to_tree, int *o_err_code);
-    int GetNumberOfArcs(void *i_fst, int *o_err_code);
+    int GetContextWidth(void *i_tree, int *o_err_code);
+    int GetCentralPosition(void *i_tree, int *o_err_code);
     void DeleteContextDependency(void *o_tree);
 } //namespace kaldi_python_tree
 } //extern "C"
