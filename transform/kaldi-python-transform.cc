f934b5c63a1230846d8b13431046f224eedf2d81
mkudinov <mikhailkudinov@gmail.com>
2017-10-03 19:47:24 +0300

    transform compiles

--- transform/kaldi-python-transform.cc
+++ transform/kaldi-python-transform.cc
@@ -7,9 +7,9 @@ namespace kaldi_python_transform
 void CmvnTransform(void *i_cmvn_stats_matrix, void *io_feature_matrix, bool i_var_norm, int *o_err_code)
 {
     *o_err_code = OK;
-    MatrixBase<double>* cmvn_stats = static_cast<MatrixBase<double>* >(i_cmvn_stats_matrix);
-    MatrixBase<float>* features = static_cast<MatrixBase<float>* >(io_feature_matrix);
-    kaldi::ApplyCmvn(*cmvn_stats, i_var_norm, feats);
+    kaldi::Matrix<double>* cmvn_stats = static_cast<kaldi::Matrix<double>* >(i_cmvn_stats_matrix);
+    kaldi::Matrix<float>* features = static_cast<kaldi::Matrix<float>* >(io_feature_matrix);
+    kaldi::ApplyCmvn(*cmvn_stats, i_var_norm, features);
 }
 
 } //namespace kaldi_python_asr_model
