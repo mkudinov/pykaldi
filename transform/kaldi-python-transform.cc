46756693fd79c769f27e7dc8f4db039167c6a12f
mkudinov <mikhailkudinov@gmail.com>
2017-10-03 17:31:39 +0300

    Transform class

---
+++ transform/kaldi-python-transform.cc
@@ -0,0 +1,86 @@
+#include "kaldi-python-feat.h"
+#include <iostream>
+extern "C"
+{
+namespace kaldi_python_asr_model
+{
+void *GetFeatureReader(char *i_specifier, int *o_err_code)
+{
+    *o_err_code = OK;
+    kaldi::RandomAccessBaseFloatMatrixReader* feature_reader = new kaldi::RandomAccessBaseFloatMatrixReader(i_specifier);
+    feature_reader->HasKey("abc");
+    return feature_reader;
+}
+
+void DeleteFeatureReader(void *o_feature_reader)
+{
+    if(o_feature_reader)
+    {
+        delete static_cast<kaldi::RandomAccessBaseFloatMatrixReader*>(o_feature_reader);
+        o_feature_reader = 0;
+    }
+}
+
+const void* ReadFeatureMatrix(char* i_key, void *i_feature_reader, int* o_n_rows, int* o_n_columns, int *o_err_code)
+{
+    *o_err_code = OK;
+    if(!i_feature_reader)
+    {
+        *o_err_code = NO_READER_EXISTS;
+        return 0;
+    }
+    kaldi::RandomAccessBaseFloatMatrixReader* feature_reader = static_cast<kaldi::RandomAccessBaseFloatMatrixReader*>(i_feature_reader);
+    std::string key(i_key);
+    if(!feature_reader->HasKey(key))
+    {
+        *o_err_code = NO_KEY;
+        return 0;
+    }
+    const kaldi::Matrix<float> &features = feature_reader->Value(key);
+    *o_n_rows = features.NumRows();
+    *o_n_columns = features.NumCols();
+    return &features;
+}
+
+void CopyFeatureMatrix(void *i_source, void *o_destination, int *o_err_code)
+{
+    *o_err_code = OK;
+    try
+    {
+        const kaldi::Matrix<float>* source = static_cast<const kaldi::Matrix<float>*>(i_source);
+        int nRows = source->NumRows();
+        int nColumns = source->NumCols();
+        for(int i = 0; i < nRows; i++)
+        {
+            std::memcpy(static_cast<float*>(o_destination) + i * nColumns, source->RowData(i), nColumns * sizeof(float));
+        }
+    }
+    catch(...)
+    {
+        *o_err_code = COPY_ERROR;
+    }
+}
+
+void *GetMatrixOfDeltaFeatures(void *i_feature_matrix,
+                      int i_order,
+                      int i_window,
+                      int *o_err_code)
+{
+    *o_err_code = OK;
+    try
+    {
+        kaldi::Matrix<float>* input_features = static_cast<kaldi::Matrix<float>* >(i_feature_matrix);
+        kaldi::DeltaFeaturesOptions options(i_order, i_window);
+        kaldi::Matrix<float>* new_features = new kaldi::Matrix<float>();
+        kaldi::ComputeDeltas(options, *input_features, new_features);
+        return new_features;
+    }
+    catch(...)
+    {
+        *o_err_code = INTERNAL_KALDI_ERROR;
+        return 0;
+    }
+}
+
+} //namespace kaldi_python_asr_model
+} //extern "C"
---
+++ transform/kaldi-python-transform.h
@@ -0,0 +1,31 @@
+// python/kaldi-python-readers.h
+// SRR
+// Mikhail Kudinov 2017
+
+#ifndef KALDI_PYTHON_FEAT
+#define KALDI_PYTHON_FEAT
+
+#include "common/kaldi-python-common.h"
+#include "../base/kaldi-common.h"
+#include "../util/kaldi-io.h"
+#include "../util/common-utils.h"
+#include "../feat/feature-functions.h"
+
+
+using namespace kaldi_python_common_errors;
+extern "C"
+{
+namespace kaldi_python_feat
+{
+void *GetFeatureReader(char *i_specifier, int *o_err_code);
+
+void DeleteFeatureReader(void *o_feature_reader);
+
+const void* ReadFeatureMatrix(char* i_key, void *i_feature_reader, int* o_n_rows, int* o_n_columns, int *o_err_code);
+
+void CopyFeatureMatrix(void *i_source, void *o_destination, int *o_err_code);
+
+void *GetMatrixOfDeltaFeatures(void *i_feature_matrix, int i_order, int i_window, int *o_err_code);
+} //namespace kaldi_python_feat
+} //extern "C"
+#endif
