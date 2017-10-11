ad9c26eca91030042a2b2ff2f1e7cac292ea854a
mkudinov <mikhailkudinov@gmail.com>
2017-10-11 17:52:09 +0300

    Transcriber is added. Very Simple.

---
+++ transcriber/transcriber.py
@@ -0,0 +1,21 @@
+class Transcriber(object):
+    def __init__(self, word_table, unknown_word_code, encoding='default'):
+        self._word_table = word_table
+        assert(unknown_word_code in self._word_table)
+        self._unk_code = self._word_table[unknown_word_code]
+        self._encoding = encoding
+
+    def transcribe(self, phrase):
+        if self._encoding != 'default':
+            phrase = phrase.encode(self._encoding)
+        return [self._word_table[word] if word in self._word_table else self._unk_code for word in phrase.split(' ') ]
+
+
+def load_word_table(path_to_word_table):
+    word_table = {}
+    for line in open(path_to_word_table, 'r'):
+        word_code = line.split(' ')
+        word = word_code[0]
+        code = int(word_code[1])
+        word_table[word] = code
+    return word_table
